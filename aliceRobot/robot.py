8006c2084b7eeaa2b0943dca1046107de52f2f5c
Duck <saheasy@gmail.com>
2021-09-01 00:27:08 -0500

    setup error handling

--- aliceRobot/robot.py
+++ aliceRobot/robot.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 from pymata4 import pymata4
 import time
 
@@ -26,17 +26,20 @@ def home():
     
     if request.method == 'POST':
         if request.form['submit'] == 'setup': 
-            global board
-            board = pymata4.Pymata4()
-            time.sleep(3)
+            try: 
+                global board
+                board = pymata4.Pymata4()
+                time.sleep(3)
 
-            board.set_pin_mode_servo(robot_pins['servo_left'])
-            board.servo_write(robot_pins['servo_left'], 87)
-            board.set_pin_mode_servo(robot_pins['servo_right'])
-            board.servo_write(robot_pins['servo_right'], 89)
-            board.set_pin_mode_analog_input(robot_pins['range'])
-            board.set_pin_mode_analog_input(robot_pins['light'])
-            board.set_pin_mode_digital_output(robot_pins['relay'])
+                board.set_pin_mode_servo(robot_pins['servo_left'])
+                board.servo_write(robot_pins['servo_left'], 87)
+                board.set_pin_mode_servo(robot_pins['servo_right'])
+                board.servo_write(robot_pins['servo_right'], 89)
+                board.set_pin_mode_analog_input(robot_pins['range'])
+                board.set_pin_mode_analog_input(robot_pins['light'])
+                board.set_pin_mode_digital_output(robot_pins['relay'])
+            except:
+                flash('Already Set up', category = 'error')
             
         if request.form['submit'] == 'leftForwards': 
             robot_data['servo_left'] = 177
@@ -57,3 +60,5 @@ def home():
         board.digital_write(robot_pins['relay'], robot_data['relay'])
         
     return render_template('index.html', values = robot_data )
+    
+    
