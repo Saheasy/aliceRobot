2722af62d6215f2a003491ee2daa2f009bcd9e03
Duck <saheasy@gmail.com>
2021-09-01 00:33:21 -0500

    added secret

--- aliceRobot/__init__.py
+++ aliceRobot/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template,request, redirect, url_for
+import secrets
 #robotFlaskExample
 def run_robot():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = secrets.token_urlsafe(10)
     from .robot import robot
     app.register_blueprint(robot, url_prefix='/')
     return app
